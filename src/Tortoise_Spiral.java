12d0fc534fd9ba119135912b06bcaa486a4e52ab
Nicholas Contreras <nicholasc2000@yahoo.com>
2016-04-20 16:08:32 -0700

    hi
    hi

---
+++ src/Tortoise_Spiral.java
@@ -0,0 +1,29 @@
+import java.awt.Color;
+
+import javax.swing.JOptionPane;
+
+import org.teachingextensions.logo.Tortoise;
+
+public class Tortoise_Spiral {
+
+	public static void main(String[] args) {
+		String in = JOptionPane.showInputDialog("");
+		String color = JOptionPane.showInputDialog("");
+		int sides = Integer.parseInt(in);
+		if (color.equals("red") || color.equals("Red")) {
+			Tortoise.setPenColor(Color.red);
+		}
+		if (color.equals("green") || color.equals("Green")) {
+			Tortoise.setPenColor(Color.green);
+		}
+		if (color.equals("blue") || color.equals("Blue")) {
+			Tortoise.setPenColor(Color.blue);
+		}
+		Tortoise.setSpeed(10);
+		for (int i = 0; i < 10000; i++) {
+			Tortoise.turn(360 / sides);
+			Tortoise.move(2 * i);
+		}
+	}
+
+}
