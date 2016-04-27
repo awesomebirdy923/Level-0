aff5c60a901f4f4351c2be73484c6ad5d7837599
TJHL <league@WTS5.attlocal.net>
2016-04-27 16:55:35 -0700

    hi
    hi

---
+++ src/My_Adventure.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class My_Adventure {
+
+	public static void main(String[] args){
+		String input1 = JOptionPane.showInputDialog("you walk into a house and there is two doors, " + "\n" + "One has a bunch of chainsaws, the other has some ice cream." + "\n" + "\n" + "which one do you choose?");
+	}
+	
+}
