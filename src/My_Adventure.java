a03ce0e20fd995065c1a503959548d85065bda27
TJHL <league@WTS5.attlocal.net>
2016-04-27 16:56:49 -0700

    hi
    hi

--- src/My_Adventure.java
+++ src/My_Adventure.java
@@ -3,7 +3,7 @@ import javax.swing.JOptionPane;
 public class My_Adventure {
 
 	public static void main(String[] args){
-		String input1 = JOptionPane.showInputDialog("you walk into a house and there is two doors, " + "\n" + "One has a bunch of chainsaws, the other has some ice cream." + "\n" + "\n" + "which one do you choose?");
+		String input1 = JOptionPane.showInputDialog("You walk into a house and there is two doors, " + "\n" + "One has a bunch of chainsaws, the other has some ice cream." + "\n" + "\n" + "Which one do you choose?");
 	}
 	
 }
